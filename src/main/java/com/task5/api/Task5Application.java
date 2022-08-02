c093d83fadd551ce65356e735ab3d1ab11b605de
Xojiakbar <87039534+XojiakbarA@users.noreply.github.com>
2022-08-03 00:26:43 +0500

    init commit

---
+++ src/main/java/com/task5/api/Task5Application.java
@@ -0,0 +1,13 @@
+package com.task5.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Task5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Task5Application.class, args);
+    }
+
+}
